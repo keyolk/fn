9736682a060a32622c040acfc7cef6c287e08c52
Tom Coupland <tom.coupland@oracle.com>
2018-06-11 15:59:50 +0100

    Adding Trigger model #1045
    Trigger struct with associated nonsense

---
+++ api/models/trigger.go
@@ -0,0 +1,113 @@
+package models
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"time"
+
+	"github.com/fnproject/fn/api/id"
+	"github.com/go-openapi/strfmt"
+)
+
+//go:generate jsonenums -type=TriggerType
+type TriggerType int
+
+const (
+	Unknown TriggerType = iota
+	HTTP
+)
+
+type Trigger struct {
+	ID         string                 `json:"id" db:"id"`
+	Name       string                 `json:"name" db:"name"`
+	AppID      string                 `json:"app_id" db:"app_id"`
+	FnID       string                 `json:"fn_id" db:"fn_id"`
+	CreatedAt  strfmt.DateTime        `json:"created_at,omitempty" db:"created_at"`
+	UpdatedAt  strfmt.DateTime        `json:"updated_at,omitempty" db:"updated_at"`
+	Type       TriggerType            `json:"type" db:"type"`
+	Source     string                 `json:"source" db:"source"`
+	Extensions map[string]interface{} `json:"extensions,omitempty" db:"extensions"`
+}
+
+func (t *Trigger) SetDefaults() {
+	if time.Time(t.CreatedAt).IsZero() {
+		t.CreatedAt = strfmt.DateTime(time.Now())
+	}
+	if time.Time(t.UpdatedAt).IsZero() {
+		t.UpdatedAt = strfmt.DateTime(time.Now())
+	}
+	if t.ID == "" {
+		t.ID = id.New().String()
+	}
+}
+
+func (t1 *Trigger) Equals(t2 *Trigger) bool {
+	eq := true
+	eq = eq && t1.ID == t2.ID
+	eq = eq && t1.Name == t2.Name
+	eq = eq && t1.AppID == t2.AppID
+	eq = eq && t1.FnID == t2.FnID
+
+	eq = eq && t1.Type == t2.Type
+	eq = eq && t1.Source == t2.Source
+	eq = eq && reflect.DeepEqual(t1.Extensions, t2.Extensions)
+
+	// NOTE: datastore tests are not very fun to write with timestamp checks,
+	// and these are not values the user may set so we kind of don't care.
+	//eq = eq && time.Time(t1.CreatedAt).Equal(time.Time(t2.CreatedAt))
+	//eq = eq && time.Time(t1.UpdatedAt).Equal(time.Time(t2.UpdatedAt))
+	return eq
+}
+
+var (
+	ErrTriggerMissingName = err{
+		code:  http.StatusBadRequest,
+		error: errors.New("Missing Trigger Name")}
+	ErrTriggerMissingAppID = err{
+		code:  http.StatusBadRequest,
+		error: errors.New("Missing Trigger AppID")}
+	ErrTriggerMissingFnID = err{
+		code:  http.StatusBadRequest,
+		error: errors.New("Missing Trigger FnID")}
+	ErrTriggerTypeUnknown = err{
+		code:  http.StatusBadRequest,
+		error: errors.New("Trigger Type Unknown")}
+	ErrTriggerMissingSource = err{
+		code:  http.StatusBadRequest,
+		error: errors.New("Missing Trigger Source")}
+	ErrHTTPTriggerExtensionsSet = err{
+		code:  http.StatusBadRequest,
+		error: errors.New("HTTP Trigger doesn't support extensions")}
+)
+
+func (t *Trigger) Validate() error {
+	if t.Name == "" {
+		return ErrTriggerMissingName
+	}
+
+	if t.AppID == "" {
+		return ErrTriggerMissingAppID
+	}
+
+	if t.FnID == "" {
+		return ErrTriggerMissingFnID
+	}
+
+	if t.Type == Unknown {
+		return ErrTriggerTypeUnknown
+	}
+
+	if t.Source == "" {
+		return ErrTriggerMissingSource
+	}
+
+	switch t.Type {
+	case HTTP:
+		if len(t.Extensions) != 0 {
+			return ErrHTTPTriggerExtensionsSet
+		}
+	}
+
+	return nil
+}
